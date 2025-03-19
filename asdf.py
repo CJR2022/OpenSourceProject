16e254cafc00473a785d2abf6039e1db0dfabdc8
CJR <wjdfbs13579@gmail.com>
2025-03-19 11:29:30 +0900

    test test

--- asdf.py
+++ asdf.py
@@ -1,13 +1,11 @@
+# 2022041015 최정륜
 # 5명의 학생의 세개의 교과목 (영어, C-언어, 파이썬)에 대하여  키보드로부터 입력받아 총점, 평균, 학점, 등수를 계산하는 프로그램 작성
-# 각 학생의 3개 교과목 성적을 입력 받아 해당 교과목들에 대한 학생 개인의 총점, 평균, 학점, 등수를 출력하도록 해봤습니다.
-# 학점은 90, 80, 70, 60 단위로 a, b, c, d, f 나눠서 부여했습니다.
 
-
-def grade(target, sub):
+def grade(stu, sub):
     if sub == "total":
-        score = target[sub] / 3
+        score = stu[sub] / 3
     else:
-        score = target[sub]
+        score = stu[sub]
     if score >= 90:
         return 'A'
     elif score >= 80:
@@ -19,32 +17,46 @@ def grade(target, sub):
     else:
         return 'F'
 
-
-def rank(stu_list, target, sub):
+def rank(stu_list, stu, sub):
     result = 1
     for i in range(students_num):
-        if target['id'] != stu_list[i]['id'] and target[sub] < stu_list[i][sub]:
+        if stu['id'] != stu_list[i]['id'] and stu[sub] < stu_list[i][sub]:
             result += 1
     return result
 
-students_num = 5
-
-def main() :
-    students = []
+def input_info(stu_list):
     for i in range(students_num):
         print("===========================")
+        s_id = input("학번 : ")
         name = input("이름 : ")
         en = int(input("영어 점수 : "))
         c = int(input("c언어 점수 : "))
         p = int(input("파이썬 점수 : "))
-        students.append({'name': name, 'en': en, 'c': c, 'p': p, 'id': i, 'total': en + c + p})
+        stu_list.append({'name': name, 'en': en, 'c': c, 'p': p, 'id': s_id})
         print("===========================\n")
 
-    for k in students:
-        print("\n===========================\n")
-        print(f"{k['name']}의 성적\n")
-        print(f"영어 : {k['en']}, 학점 : {grade(k, 'en')}, 등수 : {rank(students, k, 'en')}\n")
-        print(f"c언어 : {k['c']}, 학점 : {grade(k, 'c')}, 등수 : {rank(students, k, 'c')}\n")
-        print(f"파이썬 : {k['p']}, 학점 : {grade(k, 'p')}, 등수 : {rank(students, k, 'p')}\n")
-        print(f"총점 : {k['total']}, 평균 : {k['total'] / 3}, 종합등수 : {rank(students, k, 'total')}\n")
-    print("===========================")
+def calc_total_ave(stu):
+    total = stu['en']+stu['c']+stu['p']
+    ave = float(total / 3)
+    return total, ave
+
+def print_info(stu_list):
+    print(f"\n{'성적관리 프로그램':>20}\n")
+    print("=============================================================================\n")
+    print(f"{'학번':<8}{'이름':>4}{'영어':>8}{'C-언어':>8}"
+          f"{'파이썬':>8}{'총점':>8}{'평균':>8}{'학점':>8}{'등수':>8}\n")
+    print("=============================================================================\n")
+    for k in stu_list:
+        k['total'], k['ave'] = calc_total_ave(k)
+        k['grade'] = grade(k,'total')
+        k['rank'] = rank(stu_list,k,'total')
+        print(f"{k['id']:<8}{k['name']:>4}{k['en']:>8}{k['c']:>8}"
+              f"{k['p']:>8}{k['total']:>8}{k['ave']:>8}{k['grade']:>8}{k['rank']:>8}")
+
+
+students_num = 5
+
+def main():
+    students = []
+    input_info(students)
+    print_info(students)
